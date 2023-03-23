7ffdb99a3336726c8ee99b0f8289b54d325ec85d
4rgc <bohdan.andrii@gmail.com>
2023-03-23 00:44:10 -0400

    store mason servers in util module

--- nvim/lua/util.lua
+++ nvim/lua/util.lua
@@ -1,9 +1,13 @@
 local M = {}
 
-M.map = function (mode, lhs, rhs, desc, opts)
+M.map = function(mode, lhs, rhs, desc, opts)
   opts = opts or { silent = true }
   opts.desc = desc
   vim.keymap.set(mode, lhs, rhs, opts)
 end
+
+M.mason_servers = { 'eslint', 'lua_ls', 'tsserver', 'sqlls', 'rust_analyzer', 'pyright', 'marksman', 'jsonls', 'html',
+  'graphql', 'dockerls', 'docker_compose_language_service', 'dotls', 'cucumber_language_server', 'cssls',
+  'clangd', 'cmake', 'bashls', 'yamlls' }
 
 return M
