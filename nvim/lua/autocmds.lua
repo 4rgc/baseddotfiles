a9ee485b0013ddd1bc9a5254ec297d93eac9d1af
4rgc <bohdan.andrii@gmail.com>
2023-03-23 00:02:39 -0400

    remove coc autocmds

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,28 +1,3 @@
-
-vim.api.nvim_create_augroup("CocGroup", {})
-
--- Setup formatexpr specified filetype(s)
-vim.api.nvim_create_autocmd("FileType", {
-    group = "CocGroup",
-    pattern = "typescript,json",
-    command = "setl formatexpr=CocAction('formatSelected')",
-    desc = "Setup formatexpr specified filetype(s)."
-})
-
--- Update signature help on jump placeholder
-vim.api.nvim_create_autocmd("User", {
-    group = "CocGroup",
-    pattern = "CocJumpPlaceholder",
-    command = "call CocActionAsync('showSignatureHelp')",
-    desc = "Update signature help on jump placeholder"
-})
-
-vim.api.nvim_create_autocmd("CursorHold", {
-    group = "CocGroup",
-    command = "silent call CocActionAsync('highlight')",
-    desc = "Highlight symbol under cursor on CursorHold"
-})
-
 vim.api.nvim_create_autocmd('FileType', {
   pattern = 'markdown,snippets',
   command = 'set nowrap',
