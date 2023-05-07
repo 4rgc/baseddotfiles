61f24b6c0bed9f0fec42e1d41deffb4f7e3ed0f5
4rgc <bohdan.andrii@gmail.com>
2023-05-06 21:15:23 -0400

    fix context highlight line

--- nvim/lua/plugins/misc.lua
+++ nvim/lua/plugins/misc.lua
@@ -170,6 +170,7 @@ return {
                     'IndentBlanklineIndent4',
                     'IndentBlanklineIndent5'
                 },
+                context_char = '▎',
                 show_trailing_blankline_indent = false,
                 show_current_context = true,
                 show_current_context_start = true,
