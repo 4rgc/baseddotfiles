060335d95e1b4eb3d95316f426ed49d5f83ff052
4rgc <bohdan.andrii@gmail.com>
2023-08-20 10:54:56 -0400

    fix merge typo

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -138,7 +138,7 @@ return {
                     }),
                     null_ls.builtins.formatting.black,
                     null_ls.builtins.formatting.prettier,
-                    null_ls.builtins.diagnostics.ruff
+                    null_ls.builtins.diagnostics.ruff,
                     null_ls.builtins.diagnostics.rubocop,
                     null_ls.builtins.formatting.rubocop
                 },
