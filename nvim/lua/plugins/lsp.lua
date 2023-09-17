08d0400efb784b7ec3faae96050cc7c35577ba45
Andrii Bohdan <andrii.bohdan@autodesk.com>
2023-09-17 12:54:35 -0400

    add refactoring plugin

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -156,5 +156,33 @@ return {
         'numToStr/prettierrc.nvim',
         ft = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'html', 'css', 'sass', 'less',
             'markdown', 'telekasten', 'yaml', 'vue', 'json', 'graphql' },
-    }
+    },
+    {
+        "ThePrimeagen/refactoring.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        config = function()
+            require("refactoring").setup()
+
+            -- Refactoring mappings
+            vim.keymap.set("x", "<space>re", ":Refactor extract ")
+            vim.keymap.set("x", "<space>rf", ":Refactor extract_to_file ")
+
+            vim.keymap.set("x", "<space>rv", ":Refactor extract_var ")
+
+            vim.keymap.set({ "n", "x" }, "<space>ri", ":Refactor inline_var")
+
+            vim.keymap.set("n", "<space>rI", ":Refactor inline_func")
+
+            vim.keymap.set("n", "<space>rb", ":Refactor extract_block")
+            vim.keymap.set("n", "<space>rbf", ":Refactor extract_block_to_file")
+            vim.keymap.set(
+                { "n", "x" },
+                "<leader>rr",
+                function() require('refactoring').select_refactor() end
+            )
+        end,
+    },
 }
