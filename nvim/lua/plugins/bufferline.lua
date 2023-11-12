e55b8b388ec86b162ce330b9e386bd48981c8bd1
Andrii Bohdan <andrii.bohdan@autodesk.com>
2023-11-12 17:59:38 -0500

    fix bufferline by updating

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,6 +1,5 @@
 return {
     'akinsho/bufferline.nvim',
-    version = 'v3.*',
     dependencies = { 'nvim-tree/nvim-web-devicons', 'ofirgall/ofirkai.nvim' },
     config = function()
         require('bufferline').setup {
