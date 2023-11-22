2b65c9b82be7a042eff92b791115cf7f5d59c8a8
Andrii Bohdan <andrii.bohdan@autodesk.com>
2023-11-22 16:23:21 -0500

    add comments for option settings

--- nvim/lua/vars.lua
+++ nvim/lua/vars.lua
@@ -1,36 +1,41 @@
-vim.opt.termguicolors = true
+vim.o.termguicolors = true
+-- Relative line numbers
 vim.o.relativenumber = true
+-- Show line numbers
 vim.o.number = true
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+-- Default to 2 spaces for tabs
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+-- Expand tabs to spaces
 vim.o.expandtab = true
+-- Line wrap
 vim.o.textwidth = 120
 
 -- coc variables
 -- Some servers have issues with backup files, see #649
-vim.opt.backup = false
-vim.opt.writebackup = false
+vim.o.backup = false
+vim.o.writebackup = false
 
 -- Allow local config loading
 vim.o.exrc = true
 
 -- Having longer updatetime (default is 4000 ms = 4s) leads to noticeable
 -- delays and poor user experience
-vim.opt.updatetime = 300
+vim.o.updatetime = 300
 
 -- Always show the signcolumn, otherwise it would shift the text each time
 -- diagnostics appeared/became resolved
-vim.opt.signcolumn = "yes"
+vim.o.signcolumn = "yes"
 
 vim.o.timeout = true
 vim.o.timeoutlen = 300
 
 -- linewrap column
-vim.o.colorcolumn = 120
+vim.o.colorcolumn = "120"
 
 -- treesitter fold
 vim.o.foldmethod = 'expr'
 vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
 
 -- other setup
-vim.filetype.add({filename = {['Jenkinsfile'] = 'groovy'}})
+vim.filetype.add({ filename = { ['Jenkinsfile'] = 'groovy' } })
