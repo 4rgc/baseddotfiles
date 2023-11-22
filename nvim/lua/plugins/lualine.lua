a9caf2f81702b4df385c4420be6f8454b676bda3
Andrii Bohdan <andrii.bohdan@autodesk.com>
2023-11-22 14:19:08 -0500

    add current partial command and noice's status in statusline

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -4,6 +4,29 @@ return {
     config = function()
         local navic = require('nvim-navic')
         local ofirkai_lualine = require('ofirkai.statuslines.lualine')
+
+        -- Bottom line (statusline)
+        local sections = {
+            lualine_x = {
+                {
+                    require("noice").api.status.message.get_hl,
+                    cond = require("noice").api.status.message.has,
+                },
+                '%S',
+                {
+                    require("noice").api.status.mode.get,
+                    cond = require("noice").api.status.mode.has,
+                    color = { fg = "#ff9e64" },
+                },
+                {
+                    require("noice").api.status.search.get,
+                    cond = require("noice").api.status.search.has,
+                    color = { fg = "#ff9e64" },
+                },
+            }
+        }
+
+        -- Top filename line (winbar)
         local winbar = {
             lualine_a = {},
             lualine_b = {
@@ -26,6 +49,7 @@ return {
             lualine_y = {},
             lualine_z = {}
         }
+
         require('lualine').setup {
             options = {
                 icons_enabled = true,
@@ -35,7 +59,8 @@ return {
                 },
             },
             winbar = winbar,
-            inactive_winbar = winbar
+            inactive_winbar = winbar,
+            sections = sections
         }
     end
 }
