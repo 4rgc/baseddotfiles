5961955fc39b29b404b29fb1a9db88046369ee28
Andrii Bohdan <andrii.bohdan@autodesk.com>
2024-02-26 13:36:41 -0500

    fix completion-trigger mapping

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -47,7 +47,7 @@ return {
                 mapping = cmp.mapping.preset.insert({
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
                     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-A-S-Space>'] = cmp.mapping.complete(),
+                    ['<S-Space>'] = cmp.mapping.complete(),
                     ['<C-e>'] = cmp.mapping.abort(),
                     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
                     ["<Tab>"] = cmp.mapping(function(fallback)
