f86f2d905c97e261b8583358140e8a354af6c14d
Andrii Bohdan <andrii.bohdan@autodesk.com>
2024-06-06 15:49:33 -0400

    fix colors bugging out sometimes

---
+++ .config/nvim/lua/postconfig.lua
@@ -0,0 +1,6 @@
+-- This file is loaded at the very end. Include any code that has to be run after everything end is configured.
+
+-- Make sure to set the correct colorscheme
+-- Sometimes the colors do not display correctly
+-- Props to an4s_911 for figuring this out: https://web.archive.org/web/20240606194739/https://old.reddit.com/r/neovim/comments/19080c3/neovim_colors_malfunctioning_sometimes_works/
+vim.cmd('colorscheme ofirkai');
