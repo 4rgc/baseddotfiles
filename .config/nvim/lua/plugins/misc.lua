6a0ebf8888c8a78c5d133fc17d3023718cde0bf8
Andrii Bohdan <andrii.bohdan@autodesk.com>
2024-09-03 22:59:59 -0400

    add octo

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -164,5 +164,16 @@ return {
     config = function()
       require('baleia').setup()
     end
+  },
+  {
+    'pwntester/octo.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require "octo".setup()
+    end
   }
 }
