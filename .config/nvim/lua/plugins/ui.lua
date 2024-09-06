f5d5fb6ad394e18e7351a3a81b835ee1f53b9bc4
Andrii Bohdan <andrii.bohdan@autodesk.com>
2024-09-06 18:08:23 -0400

    remove redundant user command definition

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -26,37 +26,6 @@ return {
 
       -- the plugin hides the bottom cmdline, so we show the partial command in the statusline
       vim.opt.showcmdloc = 'statusline'
-
-      -- create a custom git commit command and redirect the output with noice
-      vim.api.nvim_create_user_command(
-        'Gc',
-        function(opts)
-          args = {}
-          for i, v in ipairs(opts.fargs) do
-            if i ~= 0 and i ~= 1 then
-              table.insert(args, v)
-            end
-          end
-          require('noice')
-              .redirect(
-                string.format(
-                -- pass all arguments from the values in the opts.fargs table to git commit
-                  'Git commit %s', table.concat(args, ' ')
-                ),
-                {
-                  {
-                    view = 'cmdline_output',
-                    filter = { event = "msg_show", kind = { "echo", "echoerr", "echomsg" } }
-                  },
-                  {
-                    view = 'notify',
-                    filter = { event = "msg_show", kind = { "echo", "echoerr", "echomsg" } }
-                  },
-                }
-              )
-        end,
-        { nargs = "*" }
-      )
     end
   },
   {
